a5e4867b13dfc672c4ca0d5b3e317f25ed6b30b5
Jesper <jesper@trell.se>
2021-01-19 16:24:00 +0100

    Simple Unit Test
    - Refactored code by creating a parser function.
    - Created a simple test for the parser function using PyTest.

--- code/project1.py
+++ code/project1.py
@@ -1,21 +1,24 @@
 import base64
 from struct import unpack
 
-# To-Do List:
-# - Write some kind of test for this code with PyTest.
 
 # Data encoded according to the base64 protocol.
 encoded_data = "NAkAALoLAAA="
 
-# Decodes the encoded data.
-decoded_data = base64.b64decode(encoded_data)
+def package_parser(package):
+    # Decodes the encoded data.
+    decoded_data = base64.b64decode(package)
 
-# Unpacks the decoded data into two 32-bit signed integers.
-temp, humid = unpack('ii', decoded_data)
+    # Unpacks the decoded data into two 32-bit signed integers.
+    temp, humid = unpack('ii', decoded_data)
 
-# Converts temperatures from significands to decimal values.
-temp_dec = temp * 10**(-2)
-humid_dec = humid * 10**(-2)
+    # Converts temperatures from significands to decimal values.
+    temp_dec = temp * 10**(-2)
+    humid_dec = humid * 10**(-2)
 
-print(f"The extracted temperature is {temp_dec}.")
-print(f"The extracted humidity is {humid_dec}.")
+    return (temp_dec, humid_dec,)
+
+temp, humid = package_parser(encoded_data)
+
+print(f"The extracted temperature is {temp}.")
+print(f"The extracted humidity is {humid}.")
